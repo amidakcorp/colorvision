0f5ecffab2fbe22cac5971566e96d6c6f778b8ce
amidakcorp <86689257+amidakcorp@users.noreply.github.com>
2021-09-14 20:54:22 +0300

    Add files via upload

---
+++ checkout.php
@@ -0,0 +1,72 @@
+<html lang="en" class="sr js anime-ready"><head><script src="chrome-extension://jfkjbfhcfaoldhgbnkekkoheganchiea/scripts/injected.js"></script>
+    
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ColorVision Cosmetics</title>
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,600" rel="stylesheet">
+    <link rel="stylesheet" href="dist/css/style.css">
+    <script src="https://unpkg.com/animejs@3.0.1/lib/anime.min.js"></script>
+    <script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></script>
+    
+    
+    
+    
+    
+    
+<link rel="stylesheet" href="css/font-awesome.css?=ogyj1jiE" id="font-awesome"><link rel="stylesheet" href="css/theme.css?=XUbfrhdm" id="theme-css"><link rel="stylesheet" href="css/custom_elements.css?=FsygbNg4" id="custom-elements-css"><link rel="stylesheet" href="css/template.css?=Tupa5Yoq" id="template-css"><link rel="stylesheet" href="css/styles.css?=40h8cJiS" id="custom-css"></head>
+
+<body class="is-boxed has-animations" style="height: 100%;">
+    <div class="body-wrap">
+
+
+        <main style="background-image: url(&quot;images/jfWRTW9xuVU0P8hB5OyUtxtRQaUfsawCwMlfLFtm.png&quot;);">
+            <section class="hero" style="background-image: url(&quot;images/QS30AhD7lbFhEzwYboGjL6Q5RnvbEbpXUgQ8TBGp.jpg&quot;); max-width: none; max-height: none; width: 909px; height: 550px;">
+                <div class="container">
+                    <div class="hero-inner">
+                        <div class="hero-copy" style="">
+                            <h1 class="hero-title mt-0">Thank You For Shopping With Us..</h1>
+                            <div class="hero-cta"><a class="button button-primary" href="index.php">Home</a><a class="button" href="#" contenteditable="true">refer and earn</a></div>
+                        </div>
+                        <div class="hero-figure anime-element">
+                            <svg class="placeholder" width="528" height="396" viewBox="0 0 528 396">
+                                <rect width="528" height="396" style="fill:transparent;"></rect>
+                            </svg>
+                            <div class="hero-figure-box hero-figure-box-01" data-rotation="45deg" style="transform: rotate(-103deg) scale(0.7); opacity: 0;"></div>
+                            <div class="hero-figure-box hero-figure-box-02" data-rotation="-45deg" style="transform: rotate(-103deg) scale(0.7); opacity: 0;"></div>
+                            <div class="hero-figure-box hero-figure-box-03" data-rotation="0deg" style="transform: rotate(-103deg) scale(0.7); opacity: 0;"></div>
+                            <div class="hero-figure-box hero-figure-box-04" data-rotation="-135deg" style="transform: rotate(-103deg) scale(0.7); opacity: 0;"></div>
+                            <div class="hero-figure-box hero-figure-box-05" style="transform: scaleX(0.05) scaleY(0.214353) perspective(107.176px);"></div>
+                            <div class="hero-figure-box hero-figure-box-06" style="transform: scaleX(0.05) scaleY(0.0304259) perspective(15.213px);"></div>
+                            <div class="hero-figure-box hero-figure-box-07" style="transform: scaleX(0.05) scaleY(0.0304259) perspective(15.213px);"></div>
+                            <div class="hero-figure-box hero-figure-box-08" data-rotation="-22deg" style="transform: rotate(-103deg) scale(0.7); opacity: 0;"></div>
+                            <div class="hero-figure-box hero-figure-box-09" data-rotation="-52deg" style="transform: rotate(-103deg) scale(0.7); opacity: 0;"></div>
+                            <div class="hero-figure-box hero-figure-box-10" data-rotation="-50deg" style="transform: rotate(-103deg) scale(0.7); opacity: 0;"></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+
+            <section class="pricing section">
+                <div class="container-sm">
+
+                </div>
+            </section>
+
+            <section class="cta section">
+
+            </section>
+        </main>
+
+
+    </div>
+
+    <script src="dist/js/main.min.js"></script>
+
+    
+    
+
+
+<script id="template-js" src="js/template.js?=cOGAm3Tx"></script><script id="custom-js" src="js/scripts.js?=WcfRypEM"></script></body></html>
